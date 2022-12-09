c7d6453334de0da63512d5ca7ead6f0f32dcd526
Herbert Perryman <herbertperryman@Herberts-MacBook-Air.local>
2022-12-09 10:00:33 -0500

    Setting up project

---
+++ Inspire/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Inspire
+//
+//  Created by Herbert Perryman on 12/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+        
+    }
+
+
+}
+
